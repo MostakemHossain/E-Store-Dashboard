9cf0496243638558d35909d44de45d9212fec949
MostakemHossain <mostakememon123@gmail.com>
2024-05-01 07:39:51 +0600

    Add Login Page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,6 @@
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
